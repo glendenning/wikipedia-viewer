e28dd0935dbcf1f821ae019b3f93fc4cc5051301
Jack <jack.glendenning@gmail.com>
2017-01-11 09:05:14 +1000

    Improve transition of the search bar on search

--- wikipediaViewer.js
+++ wikipediaViewer.js
@@ -1,8 +1,9 @@
 // ELEMENT DECLARATIONS
 const searchBar = document.getElementById('searchBar');
 const searchResults = document.getElementById('searchResults');
-const youSearchedFor = document.getElementById('searchQuery');
+const youSearchedFor = document.getElementById('youSearchedFor');
 const magnifyingGlassStick = document.getElementById('magnifyingGlassStick');
+const clickToStartTheSearch = document.getElementById('clicktostartsearch');
 
 // FUNCTIONS
 
@@ -25,7 +26,10 @@ function search() {
       	const pageTitles = data[1];
       	const pageDescriptions = data[2];
       	const pageHyperlinks = data[3];
+      	
+      	searchResults.style.opacity = 0;
       	displaySearchResults(searchedQuery, pageTitles, pageDescriptions, pageHyperlinks);
+      	searchResults.style.opacity = 1;
       },
       error: function(errorMessage) {
       	alert(errorMessage);
@@ -36,13 +40,15 @@ function search() {
 
 function displaySearchResults (query, titles, descriptions, hyperlinks) {
 
+	
+
 	var searchResultsString = "";
 	for (var i=0; i < titles.length; i++) {
 
 		var wikipediaItemString = `
 			<li >
 			  <a href="${ hyperlinks[i] }" target="blank">
-			    <p> ${ titles[i] } </p>
+			    <p class="title"> ${ titles[i] } </p>
 			    <p> ${ descriptions[i] } </p>
 			  </a>
 			</li>
@@ -50,11 +56,15 @@ function displaySearchResults (query, titles, descriptions, hyperlinks) {
 		searchResultsString += wikipediaItemString;
 	}
 
+	
+    youSearchedFor.className = ""
 	youSearchedFor.visibility = "visible";
 	searchResults.visibility = "visible";
 
 	youSearchedFor.innerHTML = "You Searched For \""+query+"\"";
 	searchResults.innerHTML = searchResultsString;
+
+	searchResults.style.opacity = 1;  //changing opacity from 0 to 1 allows it to fade in.
 
 };
 
@@ -64,16 +74,19 @@ searchBar.addEventListener("keyup", function(event) {
 	event.preventDefault();
     //if the return key is pressed (which has a keycode of 13, click the)
     if (event.keyCode == 13) {
+
     	search();
 
     	if (searchBar.className.includes("centeredSearchBar")) {
     		searchBar.className = "";
     	//the searchbar will move upwards to the top of the screen now. Want the
     	//magnifying glass stick not just be behind it. Want it hidden completely.
+    	searchBar.style.transition = "none";
     	magnifyingGlassStick.style.transition = "none"
     	magnifyingGlassStick.style.visibility = "hidden";
+    	clickToStartTheSearch.style.visibility = "hidden";
+  	    }
     }
-}
 });
 
 searchBar.addEventListener("click", function(event) {
@@ -82,6 +95,7 @@ searchBar.addEventListener("click", function(event) {
     if (searchBar.className.includes("squishedSearchBar")) {
     	searchBar.className = searchBar.className.replace("squishedSearchBar", "");
     	magnifyingGlassStick.style.width = "0px";
+    	clickToStartTheSearch.innerHTML = "Enter a Search Term to Search Wikipedia"
     };
 
 });
